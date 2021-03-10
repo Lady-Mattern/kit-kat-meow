0611c9e5670ab00bdec2b34529bb94a5e2d0311b
Leah Mattern <ladyleahmattern@gmail.com>
2021-03-10 00:18:18 -0500

    Added global variables to script.js

--- js/script.js
+++ js/script.js
@@ -1,5 +1,26 @@
 /*----- constants -----*/
+
+const PLAYERS = {
+    '1': 'X',
+    '-1': 'O',
+    'null': ''
+};
+
+const COMBOS = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6]
+];
+
 /*----- app's state (variables) -----*/
+
+let winner, turn, gameboard;
+
 /*----- cached element references -----*/
 /*----- event listeners -----*/
 /*----- functions -----*/
